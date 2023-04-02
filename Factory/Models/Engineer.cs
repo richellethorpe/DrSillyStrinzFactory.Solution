eb26524223d21e66c12bf9ebc7c16f25f0bb8467
Richelle <richelle.ak@gmail.com>
2023-04-02 09:28:53 -0700

    add validation messages to engineer and machine create views

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
-
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Factory.Models
 
@@ -7,7 +9,9 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The engineer's first name cannot be empty")]
     public string FirstName { get; set; }
+    [Required(ErrorMessage = "The engineer's last name cannot be empty")]
     public string LastName { get; set; }
 
     public string DateOfHire { get; set; }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
-
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Factory.Models
 
@@ -7,7 +9,10 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "The machine's name cannot be empty")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "The machine's description cannot be empty")]
     public string Description { get; set; }
 
     public List<EngineerMachineJoin> EngineerMachineJoins { get; }
