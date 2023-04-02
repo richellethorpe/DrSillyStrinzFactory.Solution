11bd2211584c892dcc5170eaf198dcb9cb8e0123
Richelle <richelle.ak@gmail.com>
2023-04-02 14:49:18 -0700

    update homepage to display machines and engineers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+      private readonly FactoryContext _db;
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+        Engineer[] engies = _db.Engineers.ToArray();
+        Machine[] mechs = _db.Machines.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string,object[]>();
+        model.Add("engineers", engies);
+        model.Add("machines", mechs);
+        return View(model);
     }
   }
 }
